a3d8da1fc3cfc9204753b539dc9d0b312b7afc82
Sangeet Paudel <stiletdurden252@gmail.com>
2024-12-11 17:02:39 +0545

    Final Push

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,8 +2,8 @@ import { useContext } from "react";
 
 import { CartContext } from "../store/shopping-context";
 
-export default function Cart({ onUpdateItemQuantity }) {
-  const { items } = useContext(CartContext);
+export default function Cart({}) {
+  const { items, updateItemQuantity } = useContext(CartContext);
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
@@ -25,11 +25,11 @@ export default function Cart({ onUpdateItemQuantity }) {
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                  <button onClick={() => updateItemQuantity(item.id, -1)}>
                     -
                   </button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                  <button onClick={() => updateItemQuantity(item.id, +1)}>
                     +
                   </button>
                 </div>
@@ -44,5 +44,3 @@ export default function Cart({ onUpdateItemQuantity }) {
     </div>
   );
 }
-
-
